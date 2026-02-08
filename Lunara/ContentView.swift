225f70fbabc3ad030004c22ebbf02adf1e932970
Tim Bornholdt <tim@timbornholdt.com>
2026-02-08 14:42:36 -0600

    Initial Commit

---
+++ Lunara/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lunara
+//
+//  Created by Tim Bornholdt on 2/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
