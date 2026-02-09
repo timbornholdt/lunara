7ff017f2a0c32f2472b439325853576459c61da0
Tim Bornholdt <tim@timbornholdt.com>
2026-02-09 16:51:06 -0600

    Fix UIAppFonts array to load Playfair

--- Lunara/LunaraApp.swift
+++ Lunara/LunaraApp.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 @main
 struct LunaraApp: App {
+    init() {
+        #if DEBUG
+        let playfairFonts = UIFont.familyNames
+            .filter { $0.localizedCaseInsensitiveContains("Playfair") }
+            .flatMap { family in
+                UIFont.fontNames(forFamilyName: family).map { "\(family): \($0)" }
+            }
+        print("Playfair fonts registered:", playfairFonts)
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
