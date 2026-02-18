21335ac7ac49392ac7a3baafee4bd4bbf80ebf1e
Tim Bornholdt <tim@timbornholdt.com>
2026-02-17 21:26:21 -0600

    [Phase 4] Library Tabs: theme tab bar palette and typography

--- Lunara/Views/Components/LunaraSemanticColors.swift
+++ Lunara/Views/Components/LunaraSemanticColors.swift
@@ -4,15 +4,19 @@ import UIKit
 extension Color {
     static func lunara(_ role: LunaraSemanticColorRole) -> Color {
         Color(
-            uiColor: UIColor { traits in
-                let scheme: ColorScheme = traits.userInterfaceStyle == .dark ? .dark : .light
-                return UIColor(token: LunaraVisualTokens.colorToken(for: role, in: scheme))
-            }
+            uiColor: UIColor.lunara(role)
         )
     }
 }
 
-private extension UIColor {
+extension UIColor {
+    static func lunara(_ role: LunaraSemanticColorRole) -> UIColor {
+        UIColor { traits in
+            let scheme: ColorScheme = traits.userInterfaceStyle == .dark ? .dark : .light
+            return UIColor(token: LunaraVisualTokens.colorToken(for: role, in: scheme))
+        }
+    }
+
     convenience init(token: LunaraColorToken) {
         self.init(
             red: token.red,
